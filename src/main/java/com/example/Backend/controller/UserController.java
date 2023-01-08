1f10ee0439b2df1813d3fdb7a187ae9444fccfad
Isaiah Bouromphongsa <103343585+IsaiahBouromphongsa@users.noreply.github.com>
2023-01-08 06:27:48 -0600

    Add user not found exception

--- src/main/java/com/example/Backend/controller/UserController.java
+++ src/main/java/com/example/Backend/controller/UserController.java
@@ -1,16 +1,16 @@
 package com.example.Backend.controller;
 
+import com.example.Backend.exception.UserNotFoundException;
 import com.example.Backend.model.User;
 import com.example.Backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -23,5 +23,11 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
     }
 }
